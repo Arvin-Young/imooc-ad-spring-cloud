e43a90d2888d31afd5965452a5755ad2fff707d3
arvin <792655826@qq.com>
2021-04-08 23:36:55 +0800

    imooc ad eureka

---
+++ ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by arvinyoung on 2021/4/8.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
